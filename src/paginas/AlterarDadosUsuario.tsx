7a2fb975a6a75d82d04fabdc1014341174ea1d26
Pedro henrique Pecly Gomes <135339009+pedroPecly@users.noreply.github.com>
2025-03-17 14:48:03 -0300

    atualizando carregamento de telas

--- src/paginas/AlterarDadosUsuario.tsx
+++ src/paginas/AlterarDadosUsuario.tsx
@@ -72,6 +72,14 @@ export function AlterarDadosUsuario() {
     }
   }
 
+  if (carregando) {
+    return (
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <div className="text-center">Carregando...</div>
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
